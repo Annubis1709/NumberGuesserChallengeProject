bb4c55ae92230697498ced570c84876ef7ac21d1
Edier Sanchez Sanchez <sansankind@gmail.com>
2022-07-14 18:35:37 -0500

    a functionality to check whether the user guess is between 0 and 9 is added.

--- script.js
+++ script.js
@@ -9,9 +9,14 @@ const secretNumber = generateTarget();
 const getAbsoluteDistance = (targetNumber, guessNumber) => Math.abs(targetNumber - guessNumber);
 
 const compareGuesses = (humanGuess, computerGuess, secretNumber) => {
-    const diffUser =  getAbsoluteDistance(secretNumber, humanGuess);
-    const diffComputer = getAbsoluteDistance(secretNumber, computerGuess);
-    return (diffUser <= diffComputer);
+    if (humanGuess >= 0 && humanGuess <= 9) {
+        const diffUser =  getAbsoluteDistance(secretNumber, humanGuess);
+        const diffComputer = getAbsoluteDistance(secretNumber, computerGuess);
+        return (diffUser <= diffComputer);
+    } else {
+        alert('Sorry, your number is out of range. ' +
+            'Please digit a number between 0 and 9');
+    }
 };
 
 const updateScore = (winner) => {
